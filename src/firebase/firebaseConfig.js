4b57360ae500136b63e023f5f3f9582b44e2369d
Ivaskiv <viraivaskiv@gmail.com>
2025-01-13 11:13:14 +0100

    upd

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,7 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
